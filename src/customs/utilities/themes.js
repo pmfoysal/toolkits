2ec21e4b2a24ffb63419d955128b69f1a602954f
pmfoysal <pmfoysal@gmail.com>
2022-03-23 16:20:51 +0600

    Themes Color Updated

--- src/customs/utilities/themes.js
+++ src/customs/utilities/themes.js
@@ -1,11 +1,47 @@
-import {red} from './colorPalette';
+import {green, blue} from './colorPalette';
 
 const themes = {
    white: {
       name: 'white',
+      text: {
+         main: {
+            head: '#1a1a1a',
+            title: '#333333',
+            para: '#4d4d4d',
+            hold: '#808080',
+         },
+         sub: {
+            head: '#fafafa',
+            title: '#ebebeb',
+            para: '#d9d9d9',
+            hold: '#808080',
+         },
+      },
+      back: {
+         main: blue[500],
+         sub: green[500],
+      },
    },
    black: {
       name: 'black',
+      text: {
+         main: {
+            head: '#fafafa',
+            title: '#ebebeb',
+            para: '#d9d9d9',
+            hold: '#808080',
+         },
+         sub: {
+            head: '#1a1a1a',
+            title: '#333333',
+            para: '#4d4d4d',
+            hold: '#808080',
+         },
+      },
+      back: {
+         main: blue[500],
+         sub: green[500],
+      },
    },
 };
 
