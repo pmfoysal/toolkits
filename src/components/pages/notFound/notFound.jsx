4a31ecb36f56f24a6d8a5a3a7aa451d533770a9d
pmfoysal <pmfoysal@gmail.com>
2022-05-23 13:03:17 +0600

    NotFound Page Component Added

--- src/components/pages/notFound/notFound.jsx
+++ src/components/pages/notFound/notFound.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import Button from '@shared/button';
+import errorpage from '@images/errorpage.png';
+import MainContainer from '@shared/mainContainer';
+import {NotFoundContainer} from './notFound.styled';
 
 export default function NotFound() {
-   return <div>NotFound</div>;
+   return (
+      <NotFoundContainer>
+         <MainContainer>
+            <img src={errorpage} alt='error-404' />
+            <Button name='back to home' round neutral link='/' />
+         </MainContainer>
+      </NotFoundContainer>
+   );
 }
---
+++ src/components/pages/notFound/notFound.styled.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const NotFoundContainer = styled.section`
+   width: 100%;
+   padding: 8rem 0;
+
+   & > * {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      text-align: center;
+      flex-direction: column;
+      gap: 5rem;
+
+      img {
+         width: 75rem;
+         height: auto;
+      }
+   }
+`;
