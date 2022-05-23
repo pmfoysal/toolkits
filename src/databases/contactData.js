4ee5950c223e869b9fda2d9fe47b10582f7f91b7
pmfoysal <pmfoysal@gmail.com>
2022-05-23 18:38:17 +0600

    ContactData and MenuData Added

---
+++ src/databases/contactData.js
@@ -0,0 +1,24 @@
+const contactData = [
+   {
+      id: 'contact-001',
+      icon: 'eva:phone-call-fill',
+      title: 'talk to sales',
+      para: 'Contact here if you wantent to know about products information!',
+      button: {
+         name: 'contact sales',
+         link: '',
+      },
+   },
+   {
+      id: 'contact-002',
+      icon: 'fluent:chat-bubbles-question-24-filled',
+      title: 'customer support',
+      para: 'Contact here if you wantent to know about others information!',
+      button: {
+         name: 'contact support',
+         link: '',
+      },
+   },
+];
+
+export default contactData;
---
+++ src/databases/menuData.js
@@ -0,0 +1,76 @@
+const menuData = {
+   about: [
+      {
+         name: 'about us',
+         link: '/about',
+      },
+      {
+         name: 'features',
+         link: '/features',
+      },
+      {
+         name: 'news & blogs',
+         link: '/blogs',
+      },
+      {
+         name: 'awards',
+         link: '/awards',
+      },
+   ],
+   movement: [
+      {
+         name: 'why grocery',
+         link: '/whatis',
+      },
+      {
+         name: 'support us',
+         link: '/support',
+      },
+      {
+         name: 'feedback',
+         link: '/feedback',
+      },
+      {
+         name: 'press release',
+         link: '/press',
+      },
+   ],
+   services: [
+      {
+         name: 'our services',
+         link: '/services',
+      },
+      {
+         name: 'popular inventories',
+         link: '/inventories/popular',
+      },
+      {
+         name: 'best supplier',
+         link: '/suppliers',
+      },
+      {
+         name: 'affiliate program',
+         link: '/affiliate',
+      },
+   ],
+   conditions: [
+      {
+         name: 'terms & conditions',
+         link: '/terms',
+      },
+      {
+         name: 'privacy policy',
+         link: '/privacy',
+      },
+      {
+         name: 'delivery terms',
+         link: '/terms/delivery',
+      },
+      {
+         name: 'security',
+         link: '/security',
+      },
+   ],
+};
+
+export default menuData;
