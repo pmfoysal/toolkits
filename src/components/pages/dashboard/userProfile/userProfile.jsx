a79c8f4cfb77d87ef8941f221681aa1fa8edbe41
pmfoysal <pmfoysal@gmail.com>
2022-05-24 10:22:35 +0600

    UserProfile Page Component Added

--- src/components/pages/dashboard/userProfile/userProfile.jsx
+++ src/components/pages/dashboard/userProfile/userProfile.jsx
@@ -1,5 +1,74 @@
-import React from 'react';
+import {StoreContext} from '@contexts/storeProvider';
+import ImgLoader from '@shared/imgLoader';
+import avatar from '@icons/user.svg';
+import React, {useContext, useEffect, useState} from 'react';
+import {
+   ProfileButtons,
+   ProfileInputGroup,
+   UserProfileContainer,
+   UserProfileEmail,
+   UserProfileForm,
+   UserProfileImage,
+   UserProfileName,
+   UserProfileRole,
+} from './userProfile.styled';
+import Inputbox from '@shared/inputbox';
+import Button from '@shared/button';
 
 export default function UserProfile() {
-   return <div>UserProfile</div>;
+   const {user, role} = useContext(StoreContext);
+   const [firstName, setFirstName] = useState(user?.displayName?.split(' ')[0]);
+   const [lastName, setLastName] = useState(user?.displayName?.split(' ')[1]);
+   const [location, setLocation] = useState('');
+   const [email, setEmail] = useState(user?.email);
+   const [image, setImage] = useState(user?.photoURL);
+   const [phone, setPhone] = useState('');
+   const [edit, setEdit] = useState(false);
+   const [disable, setDisable] = useState(true);
+
+   function inputHandler(setter) {
+      return function (event) {
+         setter(event.target.value);
+      };
+   }
+
+   function resetForm() {
+      setFirstName(user?.displayName?.split(' ')[0]);
+      setLastName(user?.displayName?.split(' ')[1]);
+      setLocation('');
+      setImage(user?.photoURL);
+      setPhone('');
+   }
+
+   function editHandler() {
+      setEdit(current => !current);
+      resetForm();
+   }
+
+   return (
+      <UserProfileContainer>
+         <UserProfileImage>
+            <ImgLoader src={user?.photoURL || avatar} alt='user' />
+         </UserProfileImage>
+         <UserProfileName>{user?.displayName || 'unknown'}</UserProfileName>
+         <UserProfileEmail>{user?.email}</UserProfileEmail>
+         <UserProfileRole>
+            role: <span>{role}</span>
+         </UserProfileRole>
+         <UserProfileForm>
+            <ProfileInputGroup>
+               <Inputbox name='first name' type='text' value={firstName} readOnly={!edit} handler={inputHandler(setFirstName)} />
+               <Inputbox name='last name' type='text' value={lastName} readOnly={!edit} handler={inputHandler(setLastName)} />
+            </ProfileInputGroup>
+            <Inputbox name='email address' type='email' value={email} readOnly='true' />
+            <Inputbox name='phone number' type='tel' value={phone} readOnly={!edit} handler={inputHandler(setPhone)} />
+            <Inputbox name='photo url' type='url' value={image} readOnly={!edit} handler={inputHandler(setImage)} />
+            <Inputbox name='location' type='text' value={location} readOnly={!edit} handler={inputHandler(setLocation)} />
+            <ProfileButtons>
+               <Button name={edit ? 'cancel' : 'edit'} round neutral handler={editHandler} medium />
+               <Button name='save' round medium disabled={disable} />
+            </ProfileButtons>
+         </UserProfileForm>
+      </UserProfileContainer>
+   );
 }
---
+++ src/components/pages/dashboard/userProfile/userProfile.styled.js
@@ -0,0 +1,66 @@
+import styled from 'styled-components';
+
+export const UserProfileContainer = styled.article`
+   display: flex;
+   align-items: center;
+   flex-direction: column;
+   justify-content: center;
+`;
+
+export const UserProfileImage = styled.figure`
+   height: 18rem;
+   width: 18rem;
+   border-radius: 100%;
+   overflow: hidden;
+   background-color: ${({theme}) => theme.back.card0};
+`;
+
+export const UserProfileName = styled.h1`
+   font-size: 3rem;
+   margin-top: 2rem;
+   color: ${({theme}) => theme.text.main.head};
+`;
+
+export const UserProfileEmail = styled.h3`
+   font-size: 1.6rem;
+   font-weight: 550;
+   margin-top: 0.5rem;
+   color: ${({theme}) => theme.text.main.para};
+   text-transform: lowercase;
+`;
+
+export const UserProfileRole = styled.h3`
+   font-size: 1.6rem;
+   font-weight: 550;
+   margin-top: 1rem;
+   color: ${({theme}) => theme.text.main.para};
+
+   span {
+      line-height: inherit;
+      font-size: inherit;
+      font-weight: inherit;
+      color: ${({theme}) => theme.back.accent};
+   }
+`;
+
+export const UserProfileForm = styled.div`
+   width: 60rem;
+   display: flex;
+   flex-direction: column;
+   gap: 2rem;
+   padding: 5rem 0 3rem;
+`;
+
+export const ProfileInputGroup = styled.div`
+   display: grid;
+   grid-template-columns: repeat(2, 1fr);
+   gap: 2rem;
+`;
+
+export const ProfileButtons = styled.div`
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   gap: 2rem;
+   margin-top: 2rem;
+`;
