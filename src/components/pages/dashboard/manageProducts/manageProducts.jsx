0c2a9f2efd7de374fbd4b89d5f6e4df7c5e8bfe7
pmfoysal <pmfoysal@gmail.com>
2022-05-24 16:45:18 +0600

    ManageProducts Page Component Added

--- src/components/pages/dashboard/manageProducts/manageProducts.jsx
+++ src/components/pages/dashboard/manageProducts/manageProducts.jsx
@@ -1,5 +1,47 @@
+import Button from '@shared/button';
 import React from 'react';
+import {ManageProductsBody, ManageProductsContainer, ManageProductsContent, ManageProductsHeader} from './manageProducts.styled';
 
 export default function ManageProducts() {
-   return <div>ManageProducts</div>;
+   const array = [1, 2, 3, 4, 5];
+
+   function getActions() {}
+
+   return (
+      <ManageProductsContainer>
+         <ManageProductsContent>
+            <ManageProductsHeader>
+               <tr>
+                  <th>sl no</th>
+                  <th>image</th>
+                  <th>author</th>
+                  <th>product name</th>
+                  <th>price</th>
+                  <th>required</th>
+                  <th>available</th>
+                  <th>actions</th>
+               </tr>
+            </ManageProductsHeader>
+            <ManageProductsBody>
+               {array.map((v, i) => (
+                  <tr key={i}>
+                     <td>01</td>
+                     <td>
+                        <img src='https://i.pravatar.cc/150?img=41' alt='product' />
+                     </td>
+                     <td>pmfoysal</td>
+                     <td>foysal ahmmed</td>
+                     <td>10</td>
+                     <td>20</td>
+                     <td>200</td>
+                     <td>
+                        <Button name='update' small neutral />
+                        <Button name='delete' small danger />
+                     </td>
+                  </tr>
+               ))}
+            </ManageProductsBody>
+         </ManageProductsContent>
+      </ManageProductsContainer>
+   );
 }
---
+++ src/components/pages/dashboard/manageProducts/manageProducts.styled.js
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+
+export const ManageProductsContainer = styled.article``;
+
+export const ManageProductsContent = styled.table`
+   width: 100%;
+   border-radius: 2rem;
+   overflow: hidden;
+`;
+
+export const ManageProductsHeader = styled.thead`
+   padding: 1.5rem;
+   background-color: ${({theme}) => theme.back.accent};
+
+   tr {
+      width: 100%;
+      display: grid;
+      grid-template-columns: 7rem 8rem 12rem 1fr 10rem 12rem 12rem 14rem;
+      gap: 1rem;
+      justify-items: flex-start;
+   }
+
+   th {
+      color: ${({theme}) => theme.text.main.head};
+      font-weight: 700;
+      text-transform: capitalize;
+      text-align: start;
+   }
+`;
+
+export const ManageProductsBody = styled.tbody`
+   display: flex;
+   flex-direction: column;
+   background-color: ${({theme}) => theme.back.card0};
+
+   tr {
+      padding: 0.8rem 1.5rem;
+      width: 100%;
+      display: grid;
+      grid-template-columns: 7rem 8rem 12rem 1fr 10rem 12rem 12rem 14rem;
+      gap: 1rem;
+      justify-items: flex-start;
+
+      &:nth-child(2n) {
+         background-color: ${({theme}) => theme.back.card1};
+      }
+   }
+
+   td {
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+      color: ${({theme}) => theme.text.main.para};
+      text-transform: capitalize;
+
+      img {
+         height: 3.6rem;
+         width: 3.6rem;
+         border-radius: 0.75rem;
+      }
+   }
+`;
